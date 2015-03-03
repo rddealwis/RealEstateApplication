81621e6612a1780ef63d81aec684032a69724add
rddealwis <rddealwis@gmail.com>
2015-03-03 21:18:59 +0530

    Added the project
    Added the project 'Real Estate Application'

---
+++ src/Classes/Button.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Classes;
+
+import java.awt.*;
+import javax.swing.JComponent;
+import javax.swing.plaf.ButtonUI;
+
+/**
+ *
+ * @author Ras
+ */
+public class Button extends ButtonUI{
+    
+    public void paint(Graphics g, JComponent c, String d) {
+            
+      if (d.equalsIgnoreCase("Entry"))
+      {
+         c.setForeground(Color.black);
+      } 
+      else 
+      {
+         c.setForeground(Color.white);
+      }
+
+   }
+    
+}
