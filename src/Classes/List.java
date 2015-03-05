4ac07323c7e43f1de546987cce8c12ec0649dd9a
chintakawijetunga <chintaka.wijetunga@yahoo.com>
2015-03-05 20:23:23 +0530

    real xml at startup

---
+++ src/Classes/List.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Ras
+ */
+public abstract class List {
+
+    protected Listable[] list;          // Array to hold this list’s elements
+    protected int numItems;             // Number of elements on this list
+    protected int currentPos;           // Current position for iteration
+
+    public List(int maxItems) // Instantiates and returns a reference to an empty list object 
+    // with room for maxItems elements
+    {
+        numItems = 0;
+        list = new Listable[maxItems];
+    }
+
+    public boolean isFull() // Returns whether this list is full
+    {
+        return (list.length == numItems);
+    }
+
+    public int lengthIs() // Returns the number of elements on this list 
+    {
+        return numItems;
+    }
+
+    public abstract boolean isThere(Listable item);
+// Returns true if an element with the same key as item is on this list; 
+// otherwise, returns false
+
+    public abstract Listable retrieve(Listable item);
+// Returns a copy of the list element with the same key as item
+
+    public abstract void insert(Listable item);
+// Adds a copy of item to this list 
+
+    public abstract void delete(Listable item);
+// Deletes the element with the same key as item from this list 
+
+    public void reset() // Initializes current position for an iteration through this list
+    {
+        currentPos = 0;
+    }
+
+    public Listable getNextItem() // Returns copy of the next element on this list 
+    {
+        Listable next = list[currentPos];
+        if (currentPos == numItems - 1) {
+            currentPos = 0;
+        } else {
+            currentPos++;
+        }
+        return next.copy();
+    }
+}
---
+++ src/Classes/ListHouse.java
@@ -0,0 +1,67 @@
+//----------------------------------------------------------------------------
+// ListHouse.java by Dale/Joyce/Weems Chapter 3
+//
+// Provides elements for a list of house information
+//----------------------------------------------------------------------------
+package Classes;
+
+import Classes.SortedList.*;
+
+public class ListHouse implements Listable {
+// House information
+
+    private String lastName;
+    private String firstName;
+    private int lotNumber;
+    private String price;
+    private String squareFeet;
+    private String bedRooms;
+
+    public ListHouse(String lastName, String firstName, int lotNumber,
+            String price, String squareFeet, String bedRooms) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.lotNumber = lotNumber;
+        this.price = price;
+        this.squareFeet = squareFeet;
+        this.bedRooms = bedRooms;
+    }
+
+    public Listable copy() // Returns a copy of this ListHouse
+    {
+        ListHouse result = new ListHouse(lastName, firstName, lotNumber, price,
+                squareFeet, bedRooms);
+        return result;
+    }
+
+    public int compareTo(Listable otherListHouse) // Houses are compared based on their lot numbers
+    {
+        ListHouse other = (ListHouse) otherListHouse;
+        return (this.lotNumber - other.lotNumber);
+    }
+// Observers
+
+    public String lastName() {
+        return lastName;
+    }
+
+    public String firstName() {
+        return firstName;
+    }
+
+    public int lotNumber() {
+        return lotNumber;
+    }
+
+    public String price() {
+        return price;
+    }
+
+    public String squareFeet() {
+        return squareFeet;
+    }
+
+    public String bedRooms() {
+        return bedRooms;
+    }
+}
