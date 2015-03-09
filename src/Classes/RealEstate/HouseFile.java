e3f9a0d5719954f5decb433e915fc8f202130f5d
chintakawijetunga <chintaka.wijetunga@yahoo.com>
2015-03-09 09:35:23 +0530

    Removed obsolete methods in HouseFile class

--- src/Classes/RealEstate/HouseFile.java
+++ src/Classes/RealEstate/HouseFile.java
@@ -13,49 +13,7 @@ import org.w3c.dom.*;
 public class HouseFile // Manages file "houses.dat" of real estate information
 {
 
-    private static BufferedReader inFile;
-    private static PrintWriter outFile;
-    private static boolean inFileOpen = false;
-    private static boolean outFileOpen = false;
-    private static String inString = ""; // Holds "next" line from file
-                                         // Equals null if at end of file
-
-    public static void reset() throws IOException // Reset file for reading
-    {
-        if (inFileOpen) {
-            inFile.close();
-        }
-        if (outFileOpen) {
-            outFile.close();
-        }
-        inFile = new BufferedReader(new FileReader("houses.dat"));
-        inFileOpen = true;
-        inString = inFile.readLine();
-    }
-
-    public static void rewrite() throws IOException // Reset file for writing
-    {
-        if (inFileOpen) {
-            inFile.close();
-        }
-        if (outFileOpen) {
-            outFile.close();
-        }
-        outFile = new PrintWriter(new FileWriter("houses.dat"));
-        outFileOpen = true;
-    }
-
-    public static boolean moreHouses() // Returns true if file open for reading and there is still more house
-    // information available in it
-    {
-        if (!inFileOpen || (inString == null)) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    public static ListHouse getNextHouse(Node nNode) throws IOException // Gets and returns house information from the house info file
+   public static ListHouse getNextHouse(Node nNode) throws IOException // Gets and returns house information from the house info file
     // Precondition: inFile is open and holds more house information
     {
         try
@@ -87,26 +45,4 @@ public class HouseFile // Manages file "houses.dat" of real estate information
         
     }
 
-    public static void putToFile(ListHouse house) throws IOException // Puts parameter house information into the house info file
-    // Precondition: outFile is open
-    {
-        outFile.println(house.lastName());
-        outFile.println(house.firstName());
-        outFile.println(house.lotNumber());
-        outFile.println(house.price());
-        outFile.println(house.squareFeet());
-        outFile.println(house.bedRooms());
-    }
-
-    public static void close() throws IOException // Closes house info file
-    {
-        if (inFileOpen) {
-            inFile.close();
-        }
-        if (outFileOpen) {
-            outFile.close();
-        }
-        inFileOpen = false;
-        outFileOpen = false;
-    }
 }
